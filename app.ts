dcde9e15a568d65a4d041405735e9f6a596821b7
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2018-11-30 10:20:48 +0100

    finish level1 + level3

--- app.ts
+++ app.ts
@@ -1,43 +1,91 @@
-import {CREATED, BAD_REQUEST, UNAUTHORIZED} from 'http-status-codes';
-import * as loki from 'lokijs';
-import * as express from 'express';
-import * as basic from 'express-basic-auth';
+import * as express from "express";
+import * as loki from "lokijs";
+import * as basic from "express-basic-auth";
+import * as path from "path";
 
-var app = express();
+const app = express();
+const port = parseInt(process.argv[2]) || 8080;
 app.use(express.json());
 
-const adminFilter = basic({ users: { admin: 'P@ssw0rd!' }});
+const maxGuests = 10;
 
-const db = new loki(__dirname + '/db.dat', {autosave: true, autoload: true});
-let guests = db.getCollection('guests');
-if (!guests) {
-  guests = db.addCollection('guests');
+interface IGuest {
+  firstName: string;
+  lastName: string;
 }
 
-app.get('/guests', adminFilter, (req, res) => {
-  res.send(guests.find());
-});
+interface IParty {
+  id: number;
+  title: string;
+  location: string;
+  date: string;
+  guests: IGuest[];
+}
 
-app.get('/party', (req, res, next) => {
-  res.send({
-    title: 'Happy new year!',
-    location: 'At my home',
-    date: new Date(2017, 0, 1)
-  });
-});
-
-app.post('/register', (req, res, next) => {
-  if (!req.body.firstName || !req.body.lastName) {
-    res.status(BAD_REQUEST).send('Missing mandatory member(s)');
-  } else {
-    const count = guests.count();
-    if (count < 10) {
-      const newDoc = guests.insert({firstName: req.body.firstName, lastName: req.body.lastName});
-      res.status(CREATED).send(newDoc);
-    } else {
-      res.status(UNAUTHORIZED).send('Sorry, max. number of guests already reached');
-    }
+const db = new loki(path.join(__dirname, "loki.json"), {
+  autoload: true,
+  autosave: true,
+  autosaveInterval: 100,
+  persistenceMethod: "fs",
+  autoloadCallback: () => {
+    parties = db.getCollection("parties") || db.addCollection("parties");
   }
 });
 
-app.listen(8080, () => console.log('API is listening'));
+let parties: Collection<IParty> | null = null;
+
+app.post("/party", (req, res) => {
+  const party = parties.findOne({ id: parseInt(req.params.partyId) });
+  if (!party) {
+    parties.insert({
+      id: req.body.id,
+      title: req.body.title,
+      location: req.body.location,
+      date: req.body.date,
+      guests: []
+    });
+    return res.status(201).send();
+  }
+  return res.status(409).send();
+});
+
+app.get("/party/:partyId", (req, res) => {
+  const party = parties.findOne({ id: parseInt(req.params.partyId) });
+  if (party) {
+    const { id, title, location, date } = party;
+    return res.json({ id, title, location, date });
+  }
+  res.status(404).send();
+});
+
+app.post("/party/:partyId/register", (req, res) => {
+  const party = parties.findOne({ id: parseInt(req.params.partyId) });
+
+  if (party.guests.length < maxGuests) {
+    const guest: IGuest = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName
+    };
+    if (!guest.firstName || !guest.lastName) {
+      return res.status(400).send();
+    }
+
+    parties.update({ ...party, guests: [...party.guests, guest] });
+    return res.status(201).send();
+  }
+  return res.status(401).send();
+});
+
+app.get(
+  "/party/:partyId/guests",
+  basic({ users: { admin: "P@ssw0rd!" } }),
+  (req, res) => {
+    const party = parties.findOne({ id: parseInt(req.params.partyId) });
+    if (party) {
+      return res.json(party.guests);
+    }
+    return res.status(404).send();
+  }
+);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
